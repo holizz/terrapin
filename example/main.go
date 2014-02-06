154f9fa2710ff6c305238feb8d84dc4690d92d9e
Tom Adams <tom@holizz.com>
2014-02-06 02:09:59 -0500

    Most basic command

--- example/main.go
+++ example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/codegangsta/martini"
+	"github.com/holizz/terrapin"
 	"github.com/martini-contrib/render"
 	"image"
 	"image/png"
@@ -19,6 +20,11 @@ func main() {
 
 	m.Get("/example1.png", func(w http.ResponseWriter) {
 		i := image.NewRGBA(image.Rect(0, 0, 300, 300))
+
+		t := terrapin.NewTerrapin(i, image.Pt(150, 150))
+
+		t.Forward(20)
+
 		png.Encode(w, i)
 	})
 
--- terrapin.go
+++ terrapin.go
@@ -2,16 +2,28 @@ package terrapin
 
 import (
 	"image"
+	"image/color"
 )
 
 type Terrapin struct {
-	image *image.Image
+	image *image.RGBA
+	pos image.Point
+	color color.Color
 }
 
-func NewTerrapin(i *image.Image) (t *Terrapin) {
+func NewTerrapin(i *image.RGBA, starting image.Point) (t *Terrapin) {
 	t = &Terrapin{
 		image: i,
+		pos: starting,
+		color: color.Black,
 	}
 
 	return
+}
+
+func (t *Terrapin) Forward(dist int) {
+	for i := 0; i < dist; i++ {
+		t.image.Set(t.pos.X, t.pos.Y, t.color)
+		t.pos = t.pos.Add(image.Pt(0, 1))
+	}
 }
